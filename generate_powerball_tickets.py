129a0b319811b8d6bfa19d758b6863f9e749fdea
Gino Curtis <curtis.gino@rincon.com>
2025-09-26 14:51:57 -0700

    feat: add start of powerball current jackpot value

--- generate_powerball_tickets.py
+++ generate_powerball_tickets.py
@@ -10,6 +10,7 @@ class PowerballInput:
         self.additional_inputs = [time.time()]
         self.inputs = initial_inputs + self.additional_inputs
 
+    # Add additional inputs
     def add_inputs(self):
         done = False
         while not done:
@@ -19,3 +20,25 @@ class PowerballInput:
                done = True
             else:
                 self.additional_inputs.append(user_response)
+
+    # Get powerball jackpot value
+    def get_powerball_jackpot(self):
+        headers = {
+            "User-Agent": (
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                "AppleWebKit/537.36 (KHTML, like Gecko) "
+                "Chrome/120.0.0.0 Safari/537.36"
+            )
+        }
+
+        url = "https://www.powerball.com/"
+
+        response = requests.get(url, headers=headers)
+
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        jackpot_el = soup.find("span", class_="game-jackpot-number text-xxxl lh-1 text-center")
+
+        print(jackpot_el)
+
+ 
